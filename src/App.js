2dc2cc05732dd1323c8c0e7f80abae4f55cffa36
zmr7 <zmr7@calvin.edu>
2025-02-27 14:07:17 -0500

    Initial website

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src= "Weight.png" alt='Vigil'></img>
+        <p>
+          COMING SOON...
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
